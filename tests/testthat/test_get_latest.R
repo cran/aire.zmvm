6f44a23c3a4b2b7bbcd7b59044b3a930833ad5d3
Diego Valle-Jones <diego@diegovalle.net>
2019-03-30 17:00:03 +0000

    version 0.8.2

--- tests/testthat/test_get_latest.R
+++ tests/testthat/test_get_latest.R
@@ -1,4 +1,4 @@
-
+context("Get latest IMECA values")
 
 test_that("latest data", {
   skip_on_cran()
@@ -10,4 +10,12 @@ test_that("latest data", {
   expect_false(all(is.na(df$datetime)))
 
   expect_warning(get_latest_data())
+})
+
+
+test_that("All stations in the `stations`` data.frame are up-to-date", {
+  skip_on_cran()
+  df <- get_latest_imeca()
+  expect_equal(sort(intersect(df$station_code, stations$station_code)),
+               sort(df$station_code))
 })
